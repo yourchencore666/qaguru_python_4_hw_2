def32e641812cd4eccdd8b47aa0c35cde2857866
Artem Yurchenko <yourchencore@yandex.ru>
2023-03-01 11:24:28 +0500

    homework is done

---
+++ test_python_hw.py
@@ -0,0 +1,82 @@
+import math
+import random
+
+def test_greeting():
+    """
+    Напишите программу, которая выводит на экран приветствие.
+    """
+    name = "Анна"
+    age = 25
+
+    output = f"Привет, {name}! Тебе {age} лет."
+    # Проверяем результат
+    assert output == "Привет, Анна! Тебе 25 лет."
+
+
+def test_rectangle():
+    """
+    Напишите программу, которая берет длину и ширину прямоугольника
+    и считает его периметр и площадь.
+    """
+    a = 10
+    b = 20
+
+    perimeter = (a + b) * 2
+    assert perimeter == 60
+
+    area = a * b
+    assert area == 200
+
+
+def test_circle():
+    """
+    Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
+    Используйте константу PI
+    """
+    r = 23
+    area = math.pi * pow(r, 2)
+    assert area == 1661.9025137490005
+
+    length = 2 * math.pi * r
+    assert length == 144.51326206513048
+
+
+def test_random_list():
+    """
+    Создайте список из 10 случайных чисел от 1 до 100 и отсортируйте его по возрастанию.
+    """
+    l = []
+    for i in range(1, 11):
+        l.append(random.randint(1, 100))
+
+    l.sort()
+    assert len(l) == 10
+    assert l[0] < l[-1]
+    print(l)
+
+
+def test_unique_elements():
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    for i in l:
+        if l.count(i) > 1:
+            l.remove(i)
+    assert isinstance(l, list)
+    assert len(l) == 10
+    assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_dicts():
+    """
+    Создайте словарь из двух списков.
+    Используйте первый список как ключи, а второй - как значения.
+    Выведите на экран все значения словаря.
+    Подсказка: используй встроенную функцию zip.
+    """
+    first = ["a", "b", "c", "d", "e"]
+    second = [1, 2, 3, 4, 5]
+    d = dict(zip(first, second))
+    assert isinstance(d, dict)
+    assert len(d) == 5
