de656a7d0f3992b91e5fa85b67d54b4d032d9545
Artem Yurchenko <yourchencore@yandex.ru>
2023-03-23 10:39:26 +0500

    some progress homework

---
+++ tests/models/models.py
@@ -0,0 +1,93 @@
+class Product:
+    """
+    Класс продукта
+    """
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    def __init__(self, name, price, description, quantity):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.quantity = quantity
+
+
+    def check_quantity(self, quantity) -> bool:
+        """
+        TODO Верните True если количество продукта больше или равно запрашиваемому
+            и False в обратном случае
+        """
+        return self.quantity >= quantity
+
+
+    def buy(self, quantity):
+        """
+        TODO реализуйте метод покупки
+            Проверьте количество продукта используя метод check_quantity
+            Если продуктов не хватает, то выбросите исключение ValueError
+        """
+        if self.check_quantity(quantity):
+            return Product(name=self.name, price=self.price, description=self.description, quantity=quantity)
+        else:
+            return ValueError, "Количества продуктов не хватает"
+
+
+
+    def __hash__(self):
+        return hash(self.name + self.description)
+
+class Cart:
+    """
+    Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
+    TODO реализуйте все методы класса
+    """
+
+    # Словарь продуктов и их количество в корзине
+    products: dict[Product, int]
+
+    def __init__(self):
+        # По-умолчанию корзина пустая
+        self.products = {}
+
+    def add_product(self, product: Product, quantity=1):
+        """
+        Метод добавления продукта в корзину.
+        Если продукт уже есть в корзине, то увеличиваем количество
+        """
+        if product in self.products:
+            self.products[product] += quantity
+        else:
+            self.products[product] = quantity
+        return self.products
+
+
+    def remove_product(self, product: Product, quantity=None):
+        """
+        Метод удаления продукта из корзины.
+        Если quantity не передан, то удаляется вся позиция
+        Если quantity больше, чем количество продуктов в позиции, то удаляется вся позиция
+        """
+        if quantity is None:
+            return self.products.clear(), "Позиция удалена"
+        if quantity > product.quantity:
+            return self.products.clear(), "Позиция удалена"
+
+
+    def clear(self):
+        self.products = {}
+
+
+    def get_total_price(self) -> float:
+        total_price = 0.0
+        for product in self.products:
+            total_price += product.price
+        return  total_price
+
+    def buy(self):
+        """
+        Метод покупки.
+        Учтите, что товаров может не хватать на складе.
+        В этом случае нужно выбросить исключение ValueError
+        """
