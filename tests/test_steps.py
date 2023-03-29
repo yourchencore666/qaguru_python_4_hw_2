a8db067424b30d9359bf5511583bc01c4ae88ec4
Artem Yurchenko <yourchencore@yandex.ru>
2023-03-29 13:16:58 +0500

    add allure and 3 types report

---
+++ tests/test_steps.py
@@ -0,0 +1,37 @@
+import allure
+from allure_commons.types import Severity
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+URL_GITHUB = 'https://github.com'
+REPO_NAME = 'yourchencore666/qaguru_python_4_hw_2'
+ISSUE_NUMBER = '#8'
+
+
+@allure.tag('web')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'yurchenko_a')
+@allure.feature(f'Проверка наличия Issue {ISSUE_NUMBER} на github')
+@allure.story('Лямбда шаги через with allure.step')
+@allure.link(URL_GITHUB, name='Testing')
+def test_search_github_issue():
+    with allure.step('Открываем главную страницу'):
+        browser.open(URL_GITHUB)
+
+    with allure.step(f'Поиск репозитория {REPO_NAME}'):
+        s('.header-search-input').click()
+        s('.header-search-input').send_keys(REPO_NAME)
+        s('.header-search-input').submit()
+
+    with allure.step(f'Переход по ссылке репозитория {REPO_NAME}'):
+        s(by.link_text(REPO_NAME)).click()
+
+    with allure.step('Открытие tab Issues'):
+        s('#issues-tab').click()
+
+    with allure.step(f'Проверка наличие Issue с номером {ISSUE_NUMBER}'):
+        s(by.partial_text(ISSUE_NUMBER)).should(be.visible)
+
+    browser.quit()
