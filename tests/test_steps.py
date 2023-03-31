82141327e4b97f2a1d77e0174a06f98a84b04c75
Artem Yurchenko <yourchencore@yandex.ru>
2023-03-31 12:11:14 +0500

    add selenoid

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -4,6 +4,10 @@ from selene.support import by
 from selene.support.conditions import be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from demoqa_tests import helpers
 
 URL_GITHUB = 'https://github.com'
 REPO_NAME = 'yourchencore666/qaguru_python_4_hw_2'
@@ -17,6 +21,29 @@ ISSUE_NUMBER = '#8'
 @allure.story('Лямбда шаги через with allure.step')
 @allure.link(URL_GITHUB, name='Testing')
 def test_search_github_issue():
+    options = Options()
+    options.add_argument("--window-size=1920,1080")
+
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+
+
+
     with allure.step('Открываем главную страницу'):
         browser.open(URL_GITHUB)
 
@@ -33,5 +60,9 @@ def test_search_github_issue():
 
     with allure.step(f'Проверка наличие Issue с номером {ISSUE_NUMBER}'):
         s(by.partial_text(ISSUE_NUMBER)).should(be.visible)
+
+    helpers.add_logs(browser)
+    helpers.add_screenshot(browser)
+    helpers.add_html(browser)
 
     browser.quit()
