c79a8bc726f3f061b565651a3775275588129993
Artem Yurchenko <yourchencore@yandex.ru>
2023-03-31 15:52:56 +0500

    add env

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import pytest
+from dotenv import load_dotenv
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,11 +9,17 @@ from selenium.webdriver.chrome.options import Options
 current_dir = os.path.dirname(os.path.abspath(__file__))
 resources = os.path.join(current_dir, "tests/resources")
 
+
 def pytest_addoption(parser):
     parser.addoption(
         '--browser',
         default='chrome'
     )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture
 def browser_setup():
@@ -36,10 +43,12 @@ def setup_chrome(request):
             "enableVideo": True
         }
     }
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
@@ -47,7 +56,6 @@ def setup_chrome(request):
 
 @pytest.fixture
 def create_zip():
-
     file_list = []
     for filename in os.listdir(resources):
         file_list.append(filename)
@@ -55,6 +63,7 @@ def create_zip():
     with zipfile.ZipFile("resources/archive.zip", 'w') as filezip:
         for filename in file_list:
             filezip.write(f"resources/{filename}", filename)
+
 
 @pytest.fixture
 def delete_zip():
